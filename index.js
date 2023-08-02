8c508e62129c549a28ff75ae7790348bd59ecd40
samira toubal seghir <samsidt@gmail.com>
2023-08-03 00:41:07 +0200

    Complited Assignemnts

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1=2,num2=31;
+let multiply =num1*num2;
+console.log(multiply)
+let random =Math.ceil(Math.random() );
+random != 0;
+console.log(random)
 
-// Write your code here
+
+let num3=10,num4=6;
+let mod =num3%num4;
+console.log(mod);
+
+let max=Math.max(1, 2, 3, 4, 5,20)
+console.log(max)
